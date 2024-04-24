5040c036933570832d11259cd70b10ba62e37064
jungwoo <xhdpc123@naver.com>
2024-04-24 11:26:19 +0900

    리드미 업데이트

--- test-app/midterm/src/App.js
+++ test-app/midterm/src/App.js
@@ -1,10 +1,14 @@
+App.js
+
 import logo from './logo.svg';
 import './App.css';
+import midterm from '';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <midterm></midterm>
     
     </div>
   );
---
+++ test-app/midterm/src/midterm/midterm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const styles = {
+    margin: 8,
+    padding: 8,
+    display: "flex",
+    
+
+}
+
+export default function Comment(props){
+    return (
+        <div>   
+            <h1>당신은 어떤 과일을 좋아하나요?</h1>
+        </div>
+        
+    )
+}
