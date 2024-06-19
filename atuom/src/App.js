6290300f200d73ae8638d1c7664a26edfe71b59a
pkq194 <eattato0804@gmail.com>
2024-06-19 13:53:44 +0900

    git update

---
+++ atuom/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AttendanceBook from './ch_TEST/AttendanceBook';
+
+function App() {
+  return (
+    <div className="App">
+      <AttendanceBook></AttendanceBook>
+    </div>
+  );
+}
+
+export default App;
